6e8d9afe87f1a4cec8e6fa060b52203dfd48a99f
felipemoslavacz <felipe.limah@gmail.com>
2021-08-23 19:28:47 -0300

    refactor: update map hook to react-map-gl

--- src/hooks/useMap.tsx
+++ src/hooks/useMap.tsx
@@ -1,31 +1,30 @@
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-  MutableRefObject,
-} from "react";
+import { createContext, ReactNode, useContext, useState } from "react";
 
-import mapboxgl, { Map as MapTypes } from "mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
+import { FlyToInterpolator } from "react-map-gl";
 
-import geojson, { IGeoJson } from "../services/geojson";
+import geojson, { IGeoJson, IFeatures } from "../services/geojson";
 
-import { MARKER_OPTIONS, MAX_BOUNDS } from "../config";
+interface IViewport {
+  latitude?: number;
+  longitude?: number;
+  zoom: number;
+  transitionDuration?: number;
+  transitionInterpolator?: any;
+  transitionEasing?: Function;
+}
 
 interface IMapProvider {
   children: ReactNode;
 }
 
-interface MapContextData {
-  mapContainer: MutableRefObject<HTMLDivElement | null>;
+export interface MapContextData {
+  viewport: IViewport;
+  setViewport: React.Dispatch<React.SetStateAction<IViewport>>;
   geojson: IGeoJson;
+  selectedMarker: IFeatures | null;
+  setSelectedMarker: React.Dispatch<React.SetStateAction<IFeatures | null>>;
+  handleFlyToInterpolator: Function;
 }
-
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiZ3JlZW53YXZlIiwiYSI6ImNrczY5YnVkZTE0dXoydm9kODg5djlyZWYifQ.DeQaYuUEKlS92-CIhV30AA";
 
 const MapContext = createContext({} as MapContextData);
 
@@ -39,54 +38,30 @@ export function useMap() {
 }
 
 function useProviderMap() {
-  const mapContainer = useRef(null);
-  const map = useRef<MapTypes | null>(null);
-  const [lng, setLng] = useState(-74.28);
-  const [lat, setLat] = useState(42.811);
-  const [zoom, setZoom] = useState(5);
-
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/light-v10",
-      center: [lng, lat],
-      zoom: zoom,
-      maxBounds: MAX_BOUNDS,
-    });
-    map.current.addControl(new mapboxgl.NavigationControl());
-
-    geojson.farmers.forEach(function (marker) {
-      if (!map.current) return;
-
-      new mapboxgl.Marker(MARKER_OPTIONS)
-        .setLngLat(marker.geometry.coordinates)
-        .setPopup(
-          new mapboxgl.Popup({ offset: 25 }) // add popups
-            .setHTML(
-              `<h3>${marker.properties.title}</h3>
-              <p>${marker.properties.location}</p>
-              <p>${marker.properties.description}</p>
-              <a href="#">View Profile ></a>`
-            )
-        )
-        .addTo(map.current);
-    });
+  const [viewport, setViewport] = useState<IViewport>({
+    latitude: 42.811,
+    longitude: -74.28,
+    zoom: 5,
   });
+  const [selectedMarker, setSelectedMarker] = useState<IFeatures | null>(null);
 
-  useEffect(() => {
-    if (!map.current) return; // wait for map to initialize
-    map.current.on("move", () => {
-      if (map.current === null) return;
-      setLng(Number(map.current.getCenter().lng.toFixed(4)));
-      setLat(Number(map.current.getCenter().lat.toFixed(4)));
-      setZoom(Number(map.current.getZoom().toFixed(2)));
+  function handleFlyToInterpolator(feature: IFeatures) {
+    setViewport({
+      ...viewport,
+      longitude: feature?.geometry.coordinates[0],
+      latitude: feature?.geometry.coordinates[1],
+      zoom: 8,
+      transitionDuration: 1000,
+      transitionInterpolator: new FlyToInterpolator({ speed: 2 }),
     });
-  });
+  }
 
   return {
-    mapContainer,
+    viewport,
+    setViewport,
     geojson,
+    selectedMarker,
+    setSelectedMarker,
+    handleFlyToInterpolator,
   };
 }
