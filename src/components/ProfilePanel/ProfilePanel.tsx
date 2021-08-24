6c3e77975e1862aa70db25322c1d9bdb1d2e85f5
felipemoslavacz <felipe.limah@gmail.com>
2021-08-24 20:04:18 -0300

    refactor: show message when there is no farmers

--- src/components/ProfilePanel/ProfilePanel.tsx
+++ src/components/ProfilePanel/ProfilePanel.tsx
@@ -13,25 +13,29 @@ const ProfilePanel = () => {
 
   return (
     <ProfileContainer>
-      {profiles.features.map((feature) => {
-        const {
-          id,
-          properties: { title, location, description },
-        } = feature;
-        return (
-          <ProfileItem
-            key={id}
-            title={title}
-            location={location}
-            description={description}
-            active={selectedMarker?.id === id ? "active" : ""}
-            onClick={() => {
-              setSelectedMarker(feature);
-              handleFlyToInterpolator(feature);
-            }}
-          />
-        );
-      })}
+      {profiles.features.length > 0 ? (
+        profiles.features.map((feature) => {
+          const {
+            id,
+            properties: { title, location, description },
+          } = feature;
+          return (
+            <ProfileItem
+              key={id}
+              title={title}
+              location={location}
+              description={description}
+              active={selectedMarker?.id === id ? "active" : ""}
+              onClick={() => {
+                setSelectedMarker(feature);
+                handleFlyToInterpolator(feature.geometry.coordinates);
+              }}
+            />
+          );
+        })
+      ) : (
+        <h5>Cannot find farmer profiles.</h5>
+      )}
     </ProfileContainer>
   );
 };
