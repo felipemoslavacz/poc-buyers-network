ce385f7e3fef4ef7c892aed69627cf6c81bc39d8
felipemoslavacz <felipe.limah@gmail.com>
2021-08-24 13:15:28 -0300

    refactor: cleanup

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,3 @@
-import { LngLatBoundsLike } from "mapbox-gl";
-
 export const MARKER_OPTIONS = {
   color: "#565656",
   scale: 1,
@@ -8,4 +6,4 @@ export const MARKER_OPTIONS = {
 export const MAX_BOUNDS = [
   [-82.7778, 37.07214], // New York
   [-65.32614, 48.09228], // Maine
-] as LngLatBoundsLike;
+];
